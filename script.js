3349c7090016d4524bbb06667061a1f0b5c80c4b
Fion-y <s10247592@connect.np.edu.sg>
2022-12-04 18:10:46 +0800

    all completed!

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('snsd tgt.jpg')";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover";
+  }
